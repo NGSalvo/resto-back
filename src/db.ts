42204c8d196cfe55d9db7ab17263f4f7248eed7a
GiancarloG <giancarlogravagnatrabajo@gmail.com>
2023-07-05 15:47:56 -0300

    database config secure

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,11 @@
+require('dotenv').config();
 import { connect } from 'mongoose';
+
+const { MONGOUSER, MONGOPASS, MONGODB } = process.env;
 
 export const connectDB = async () => {
   const db = await connect(
-    'mongodb+srv://main:eYexWHjVsquZMIBw@cluster9.lua1z.mongodb.net/resto?retryWrites=true&w=majority',
+    `mongodb+srv://${MONGOUSER}:${MONGOPASS}@cluster9.lua1z.mongodb.net/${MONGODB}?retryWrites=true&w=majority`,
   );
   console.log(`DB "${db.connection.db.databaseName}" connection successful`);
 };
