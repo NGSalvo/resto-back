72ccd116f4fdee41f2bf82dbe122472aaae4def1
GiancarloG <giancarlogravagnatrabajo@gmail.com>
2023-07-05 15:12:34 -0300

    database connect

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import { connect } from 'mongoose';
 
 export const connectDB = async () => {
-  const db = await connect('mongodb://localhost/restodb');
+  const db = await connect(
+    'mongodb+srv://main:eYexWHjVsquZMIBw@cluster9.lua1z.mongodb.net/resto?retryWrites=true&w=majority',
+  );
   console.log(`DB "${db.connection.db.databaseName}" connection successful`);
 };
