8b142f82363f5ced8d4c5cb4bcb75e4f76f6b379
GiancarloG <giancarlogravagnatrabajo@gmail.com>
2023-07-07 17:29:27 -0300

    Fix UpdateUsersById

--- src/controllers/UserControllers/updateUsersById.ts
+++ src/controllers/UserControllers/updateUsersById.ts
@@ -1,10 +1,12 @@
 import { Response, Request } from 'express';
 import { updateUser } from '../../services/UsersServices/updateUsers';
+import { getUserById } from '../../services/UsersServices/getUsersById';
 
 export const updateUserById = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const userById = await updateUser(id, req.body);
+    await updateUser(id, req.body);
+    const userById = await getUserById(id);
     res.send(userById);
   } catch (error) {
     console.log(error);
