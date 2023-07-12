50565bf1f82e43ca626700532ab3ccaf64d7d4bb
Nicolás Salvo <nicosalvo@gmail.com>
2023-07-12 14:27:17 -0300

    fix: change to a provisioned db of mongo

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,13 @@
 import { connect } from 'mongoose';
 
-import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } from './config';
+import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } from './config';
+
+console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT);
 
 export const connectDB = async () => {
   const db = await connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+    // `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+    `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
   );
   console.log(`DB "${db.connection.db.databaseName}" connection successful`);
 };
