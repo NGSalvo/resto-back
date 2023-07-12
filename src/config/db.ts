87aab9ec71aa72c41dcdce92e083263af02a9bd6
Nicolás Salvo <nicosalvo@gmail.com>
2023-07-12 17:09:44 -0300

    fix: testing render deplot

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,13 @@
 import { connect } from 'mongoose';
-import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } from './config';
+import { DB_URL } from './config';
+// import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_URL } from './config';
+
 // import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } from './config';
 
 export const connectDB = async () => {
   const db = await connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+    DB_URL,
+    // `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
     // `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
   );
   console.log(`DB "${db.connection.db.databaseName}" connection successful`);
