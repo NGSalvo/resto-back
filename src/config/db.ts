5eb067124fd4e676e3936dbe8299df42bac1715b
Nicolás Salvo <nicosalvo@gmail.com>
2023-07-12 15:43:03 -0300

    restore to previouse change

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,12 @@
 import { connect } from 'mongoose';
 
-import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } from './config';
-
-console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT);
+import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } from './config';
+// import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } from './config';
 
 export const connectDB = async () => {
   const db = await connect(
-    // `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
-    `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+    // `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
   );
   console.log(`DB "${db.connection.db.databaseName}" connection successful`);
 };
