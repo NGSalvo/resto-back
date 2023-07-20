9a89449e711d6bb0a2dab9f4210242aff9e75292
NGSalvo <43046596+NGSalvo@users.noreply.github.com>
2023-07-20 18:16:11 -0300

    feat: get user role (#44)
    refactor: error messages from permission

---
+++ src/services/UsersServices/getUserRole.ts
@@ -0,0 +1,15 @@
+import admin from '../../config/firebase-config';
+
+export async function getUserRole(userEmail: string) {
+  const userSnapshot = await admin
+    .firestore()
+    .collection('users')
+    .where('email', '==', userEmail)
+    .get();
+
+  if (!userSnapshot.docs[0].exists) {
+    return {};
+  }
+
+  return (userSnapshot.docs[0].data() as any)?.role;
+}
