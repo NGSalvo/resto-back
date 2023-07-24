e6d344af5a904fa699fe6a87074c4ef68426368d
NGSalvo <43046596+NGSalvo@users.noreply.github.com>
2023-07-23 22:33:55 -0300

    refactor: routes directory (#51)

--- src/routes/dish.routes.ts
+++ src/routes/dish.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 // import { schemaValidation } from '../../middlewares/schemaValidator.middleware';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { createDishSchema, updateDishSchema } from '../../schemas';
+import { createDishSchema, updateDishSchema } from '../schemas';
 import {
   postDish,
   getDish,
   getDishID,
   updateDishById,
   toggleDishById,
-} from '../../controllers';
+} from '../controllers';
 // import {
 //   decodeToken,
 //   requireAdmin,
